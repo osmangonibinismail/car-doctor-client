46555b11fbe33cb4fe755a6830816e6182a4f3c9
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-05-10 17:29:52 +0600

    9.58 sec

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import app from "../firebase/firebase.config";
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
@@ -23,12 +24,24 @@ const AuthProvider = ( {children} ) => {
         setLoading(true);
         return signOut(auth);
     }
+// 
+// 9.58sec er por vedio deka hoi nai  module--61.2
+
+
 
     useEffect( () => {
         const unsubscribe = onAuthStateChanged(auth, currentUser =>{
             setUser(currentUser);
             console.log('current user', currentUser);
             setLoading(false);
+            // if user exists then issue a token
+            if(currentUser){
+                const loggedUser = {email: currentUser.email};
+                axios.post( 'http://localhost:5000/jwt', loggedUser, {withCredential: true})
+                .then(res => {
+                    console.log('token response', res.data);
+                })
+            }
         });
         return () => {
             return unsubscribe();
